fd941ff04c448d7ce070a1a5cd208e2623067784
Charlie Wang <wangch@garena.com>
2022-01-03 16:42:34 +0800

    Default layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" class="logo">
+  <p>{{ title }}</p>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {
+  defineComponent, ref,
+} from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
+  setup() {
+    const title = ref('Notebook');
+
+    return {
+      title,
+    };
   },
 });
 </script>
 
 <style lang="scss">
 #app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 60px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  .logo {
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
+  }
 }
 </style>
